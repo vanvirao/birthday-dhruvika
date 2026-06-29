fa68926e9dc3dff2ca4ba4fc5cbda22d5170e8f6
vanvirao <vanvirao@gmail.com>
2026-06-30 00:31:29 +0530

    Refactor greeting logic for better clarity

--- src/components/LoginScreen.tsx
+++ src/components/LoginScreen.tsx
@@ -27,9 +27,13 @@ const hour = new Date().getHours();
 
 let greeting = 'Good Night';
 
-if (hour >= 5 && hour < 12) greeting = 'Good Morning';
-else if (hour < 17) greeting = 'Good Afternoon';
-else if (hour < 21) greeting = 'Good Evening';
+if (hour >= 5 && hour < 12) {
+  greeting = 'Good Morning';
+} else if (hour >= 12 && hour < 17) {
+  greeting = 'Good Afternoon';
+} else if (hour >= 17 && hour < 21) {
+  greeting = 'Good Evening';
+}
   return (
     <AnimatePresence>
       {visible && (
